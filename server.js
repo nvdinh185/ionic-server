7fd45cd221e679fe8c2e7b1ebe1c8a7cd040ef2e
nvdinh185 <nvdinh0766777123@gmail.com>
2020-09-25 16:22:53 +0700

    server ok

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-app.use(express.static(__dirname + 'client/www'));
+app.use(express.static(__dirname + '/www'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/www', 'index.html'));
+    res.sendFile(path.join(__dirname, 'www', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080, () => console.log("Server is running..."));
+const httpServer = require('http').createServer(app);
+httpServer.listen(8080, () => {
+    console.log("Server HTTP is started");
+});
