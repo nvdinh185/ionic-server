a5a154f61cc8f4ba36f0b8187c528d9665940def
nvdinh185 <nvdinh0766777123@gmail.com>
2020-09-25 16:55:54 +0700

    add socket io

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'www', 'index.html'));
 });
 
-const httpServer = require('http').createServer(app);
-httpServer.listen(8080, () => {
-    console.log("Server HTTP is started");
+const server = require('http').Server(app);
+const io = require("socket.io")(server);
+server.listen(8080, () => { console.log("Server is starting...") });
+
+io.on("connection", function (socket) {
+    console.log("Co nguoi ket noi! " + socket.id);
+    socket.on("disconnect", function () {
+        console.log(socket.id + " ngat ket noi!!!");
+    });
 });
---
+++ www/home-home-module-es5.js
@@ -0,0 +1,156 @@
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["home-home-module"], {
+  /***/
+  "./node_modules/raw-loader/dist/cjs.js!./src/app/home/home.page.html":
+  /*!***************************************************************************!*\
+    !*** ./node_modules/raw-loader/dist/cjs.js!./src/app/home/home.page.html ***!
+    \***************************************************************************/
+
+  /*! exports provided: default */
+
+  /***/
+  function node_modulesRawLoaderDistCjsJsSrcAppHomeHomePageHtml(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony default export */
+
+
+    __webpack_exports__["default"] = "<ion-header>\r\n  <ion-toolbar>\r\n    <ion-title>\r\n      Ionic Blank\r\n    </ion-title>\r\n  </ion-toolbar>\r\n</ion-header>\r\n\r\n<ion-content>\r\n  <div>\r\n    The world is your oyster.\r\n    <p>If you get lost, the <a target=\"_blank\" rel=\"noopener\" href=\"https://ionicframework.com/docs/\">docs</a> will be your guide.</p>\r\n  </div>\r\n</ion-content>\r\n";
+    /***/
+  },
+
+  /***/
+  "./src/app/home/home.module.ts":
+  /*!*************************************!*\
+    !*** ./src/app/home/home.module.ts ***!
+    \*************************************/
+
+  /*! exports provided: HomePageModule */
+
+  /***/
+  function srcAppHomeHomeModuleTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "HomePageModule", function () {
+      return HomePageModule;
+    });
+    /* harmony import */
+
+
+    var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! tslib */
+    "./node_modules/tslib/tslib.es6.js");
+    /* harmony import */
+
+
+    var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! @angular/core */
+    "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+    /* harmony import */
+
+
+    var _ionic_angular__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+    /*! @ionic/angular */
+    "./node_modules/@ionic/angular/__ivy_ngcc__/dist/fesm5.js");
+    /* harmony import */
+
+
+    var _angular_router__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+    /*! @angular/router */
+    "./node_modules/@angular/router/__ivy_ngcc__/fesm2015/router.js");
+    /* harmony import */
+
+
+    var _home_page__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(
+    /*! ./home.page */
+    "./src/app/home/home.page.ts");
+
+    var HomePageModule = function HomePageModule() {
+      _classCallCheck(this, HomePageModule);
+    };
+
+    HomePageModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+      imports: [_ionic_angular__WEBPACK_IMPORTED_MODULE_2__["IonicModule"], _angular_router__WEBPACK_IMPORTED_MODULE_3__["RouterModule"].forChild([{
+        path: '',
+        component: _home_page__WEBPACK_IMPORTED_MODULE_4__["HomePage"]
+      }])],
+      declarations: [_home_page__WEBPACK_IMPORTED_MODULE_4__["HomePage"]]
+    })], HomePageModule);
+    /***/
+  },
+
+  /***/
+  "./src/app/home/home.page.scss":
+  /*!*************************************!*\
+    !*** ./src/app/home/home.page.scss ***!
+    \*************************************/
+
+  /*! exports provided: default */
+
+  /***/
+  function srcAppHomeHomePageScss(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony default export */
+
+
+    __webpack_exports__["default"] = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2hvbWUvaG9tZS5wYWdlLnNjc3MifQ== */";
+    /***/
+  },
+
+  /***/
+  "./src/app/home/home.page.ts":
+  /*!***********************************!*\
+    !*** ./src/app/home/home.page.ts ***!
+    \***********************************/
+
+  /*! exports provided: HomePage */
+
+  /***/
+  function srcAppHomeHomePageTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "HomePage", function () {
+      return HomePage;
+    });
+    /* harmony import */
+
+
+    var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! tslib */
+    "./node_modules/tslib/tslib.es6.js");
+    /* harmony import */
+
+
+    var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! @angular/core */
+    "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+
+    var HomePage = function HomePage() {
+      _classCallCheck(this, HomePage);
+    };
+
+    HomePage = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+      selector: 'app-home',
+      template: Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(
+      /*! raw-loader!./home.page.html */
+      "./node_modules/raw-loader/dist/cjs.js!./src/app/home/home.page.html"))["default"],
+      styles: [Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"])(__webpack_require__(
+      /*! ./home.page.scss */
+      "./src/app/home/home.page.scss"))["default"]]
+    }), Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"])("design:paramtypes", [])], HomePage);
+    /***/
+  }
+}]);
+//# sourceMappingURL=home-home-module-es5.js.map
